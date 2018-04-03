1f17d0f117ec1e0a173cbc2f7c07add3ffa51e45
Mic Hussey <Michael.Hussey@solace.com>
2018-04-03 18:59:16 +0200

    Removed unused imports

--- src/main/java/com/solace/spring_cloud_stream/sample/Kinesis2SolaceBridge.java
+++ src/main/java/com/solace/spring_cloud_stream/sample/Kinesis2SolaceBridge.java
@@ -13,7 +13,6 @@ import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 
-import com.solace.spring_cloud_stream.binder.InputMessageChannelAdapter;
 import com.solace.spring_cloud_stream.binder.SolaceBinderConstants;
 
 @EnableBinding(Processor.class)
